a6ef20f6cddb0b7c06ab1e7b9d02b89d0b134ded
Anthony Serquen <srquen77@gmail.com>
2023-07-18 12:09:19 -0500

    slider arreglado

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,58 +1,64 @@
-const nextSlide = (container, i, sliders, prevItem) => {
+const drawPagination = (container, sliders) => {
   const ContainerButton = document.createElement('div');
   for (let i = 0; i < sliders.length; i++) {
     ContainerButton.innerHTML += `
-            <a href="" class="${container.className}__pagina"></a>
+            <a href="" class="${container.className}__pagina ${i === 0 ? 'active' : ''}"></a>
       `
   }
 
   ContainerButton.classList.add(`${container.className}__paginacion`)
   container.appendChild(ContainerButton);
+}
 
+const onSlide = (container, i, sliders, prevItem, buttons) => {
   document.querySelector(`.${container.className}__paginacion`).addEventListener('click', (e) => {
     e.preventDefault()
 
     if (e.target.tagName === "A") {
-      const buttons = [...document.querySelectorAll(`.${container.className}__pagina`)];
+      buttons = [...document.querySelectorAll(`.${container.className}__pagina`)];
       const clicked = buttons.indexOf(e.target)
       for (let i = 0; i < sliders.length; i++) {
-        sliders[i].style.display = 'none';
+        // sliders[i].style.display = 'none';
         buttons[i].classList.remove('active');
       }
 
       let actual = sliders[clicked];
       i = clicked;
       actual.style.display = 'block';
+      container.firstElementChild.insertAdjacentElement('beforeend', actual)
       buttons[clicked].classList.add('active');
     }
   })
 
+  // document.querySelectorAll(`.${container.className}__pagina`)[0].click()
+  
+  // setInterval(() => {
+  //   if (i === sliders.length - 1) {
+  //     i = 0;
+  //     prevItem = sliders[sliders.length - 1]
+  //   } else {
+  //     i++;
+  //     prevItem = sliders[i - 1];
+  //   }
 
-  setInterval(() => {
-    if (i === sliders.length - 1) {
-      i = 0;
-      prevItem = sliders[sliders.length - 1]
-    } else {
-      i++;
-      prevItem = sliders[i - 1];
-    }
-
-    sliders[i].style.display = 'block';
-    prevItem.style.display = 'none';
-  }, 5000)
+  //   document.querySelectorAll(`.${container.className}__pagina`)[i].click()
+  //   // sliders[i].style.display = 'block';
+  //   // prevItem.style.display = 'none';
+  // }, 5000)
 };
 
 
 const slider = (container) => {
   let i = 0;
-  const sliders = container.firstElementChild.children;
+  const sliders = [...container.firstElementChild.children];
   const prevItem = sliders[sliders.length - 1]
-  // if(sliders.className.contains())
+  const buttons = []
   sliders[0].style.display = 'block';
-  prevItem.style.display = 'none';
+  // prevItem.style.display = 'none';
 
-  console.log(sliders);
-  nextSlide(container, i, sliders, prevItem);
+  drawPagination(container, sliders);
+  onSlide(container, i, sliders, prevItem, buttons);
+  
 }
 
 export default slider;
