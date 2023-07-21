67da38f6fdcb640d0caa46ca01fa06b7c213d88f
Anthony Serquen <srquen77@gmail.com>
2023-07-21 12:41:37 -0500

    Carrousel funcionando, funciona con mas de un carrousel en la misma pagina, cards con diseño, quizas falte mejorar un poco el codigo sass

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,18 +1,55 @@
-const carousel = (container) => {
-    const buttonnext = container.querySelector('.next');
-    const buttonprev = container.querySelector('.prev');
+const carrousel = (container) => {
+    container.addEventListener('click', e => {
+        let btn = e.target
+        let track = btn.parentElement;
+        let containerCa = track.querySelector(".Cards__container");
+        let trackWidth = track.offsetWidth;
+        let containerCaWidth = containerCa.offsetWidth;
+        let leftPosition = containerCa.style.left == "" ? 0 : parseFloat(containerCa.style.left.slice(0, -2) * - 1);
 
-    const cardsContainer = container.querySelector('.Cards__container');
-    const cards = container.querySelectorAll('.Card');
-    const cardswidth = cards[0].offsetWidth;
-    const cardsContainerwidth = cardsContainer.offsetWidth;
-
-    buttonnext.addEventListener('click', e => {
-        
+        if(btn.className == 'next') {
+            if(leftPosition < (containerCaWidth - trackWidth)) {
+                leftPosition += 237 + 16;
+               containerCa.style.left = `-${leftPosition}px` 
+            } else {
+                containerCa.style.left = 0
+            }
+        } else if(btn.className == 'prev') {
+            if(leftPosition > 0) {
+                console.log(leftPosition);
+                leftPosition -= 237 + 16;
+               containerCa.style.left = `-${leftPosition}px` 
+            }
+        }
     })
-
     
-    
-}
+  }
 
-export default carousel;
+//   const carrousel = (container) => {
+//     let track = container.querySelector('.Cards__track');
+//     let containerCa = container.querySelector('.Cards__container');
+//     let buttonsContainer = container.querySelector(".Cards__navigate");
+//     let leftPosition = 0;
+
+
+//     buttonsContainer.addEventListener('click', e => {
+//         let trackWidth = track.offsetWidth;
+//         let containerCaWidth = containerCa.offsetWidth;
+
+//         if(e.target.className == 'next') {
+//             if(leftPosition < (containerCaWidth - trackWidth)) {
+//                 leftPosition += 235 + 16;
+//                containerCa.style.left = `-${leftPosition}px` 
+//               }
+//         } else if(e.target.className == 'prev') {
+//             if(leftPosition > 0) {
+//                 console.log(leftPosition);
+//                 leftPosition -= 235 + 16;
+//                containerCa.style.left = `-${leftPosition}px` 
+//               }
+//         }
+//     })
+    
+//   }
+
+export default carrousel;
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,4 +2,4 @@ import slider from "./slider.js";
 import carousel from "./carousel.js";
 
 slider(document.querySelector('.Banner'))
-carousel(document.querySelector('.Cards'));
+carousel(document.querySelector('main'));
