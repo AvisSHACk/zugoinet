1d760db1018cf1b916a13452b1292b7cac0be603
Anthony Serquen <srquen77@gmail.com>
2023-08-14 09:11:07 -0500

    Reeditar el banner en respponsive, no logro el resultado deseado

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,7 +2,7 @@ import slider from "./slider.js";
 import carousel from "./carousel.js";
 import openCreateList from "./openCreateList.js";
 
-// carousel(document.querySelector('main'));
+carousel(document.querySelector('main'));
 
 if (window.location.pathname === '/' || window.location.pathname === "/index.html") {
     slider(document.querySelector('.Banner'))
